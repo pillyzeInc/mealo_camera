798c04bde231754d4f761894201ddfdc8da9c24f
harry <95003118+HarryPillyze@users.noreply.github.com>
2025-03-26 15:37:16 +0900

    (ios) image compress

--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTSavePhotoDelegate.m
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTSavePhotoDelegate.m
@@ -36,11 +36,45 @@
     typeof(self) strongSelf = weakSelf;
     if (!strongSelf) return;
 
-    NSObject<FLTWritableData> *data = photoDataProvider();
-    NSError *ioError;
-    if ([data writeToFile:strongSelf.path options:NSDataWritingAtomic error:&ioError]) {
-      strongSelf.completionHandler(self.path, nil);
+    NSData *photoData = photoDataProvider();
+    UIImage *image = [UIImage imageWithData:photoData];
+    
+    if (image) {
+      CGFloat maxWidth = 1000.0;
+      CGFloat scale = 1.0;
+      
+      if (image.size.width > maxWidth) {
+        scale = maxWidth / image.size.width;
+      }
+      
+      if (scale < 1.0) {
+        CGSize newSize = CGSizeMake(image.size.width * scale, image.size.height * scale);
+        UIGraphicsBeginImageContextWithOptions(newSize, NO, 1.0);
+        [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+        UIImage *resizedImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        // 압축된 이미지를 JPEG로 저장
+        NSData *compressedData = UIImageJPEGRepresentation(resizedImage, 0.8);
+        NSError *ioError;
+        if ([compressedData writeToFile:strongSelf.path options:NSDataWritingAtomic error:&ioError]) {
+          strongSelf.completionHandler(self.path, nil);
+        } else {
+          strongSelf.completionHandler(nil, ioError);
+        }
+      } else {
+        // 원본 크기가 1000px 이하인 경우 그대로 저장
+        NSError *ioError;
+        if ([photoData writeToFile:strongSelf.path options:NSDataWritingAtomic error:&ioError]) {
+          strongSelf.completionHandler(self.path, nil);
+        } else {
+          strongSelf.completionHandler(nil, ioError);
+        }
+      }
     } else {
+      NSError *ioError = [NSError errorWithDomain:NSCocoaErrorDomain
+                                           code:NSURLErrorUnknown
+                                       userInfo:@{NSLocalizedDescriptionKey: @"Failed to create image from photo data"}];
       strongSelf.completionHandler(nil, ioError);
     }
   });
@@ -58,4 +92,10 @@
 - (NSString *)filePath {
   return self.path;
 }
+
+ - (void)captureOutput:(AVCapturePhotoOutput *)output willCapturePhotoForResolvedSettings:(AVCaptureResolvedPhotoSettings *)resolvedSettings
+ API_AVAILABLE(ios(10.0)){
+     AudioServicesDisposeSystemSoundID(1108);
+ }
+ 
 @end
