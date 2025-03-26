d7c9e39ebaa1d63b913146cb89807bc18cfdd1c8
harry <95003118+HarryPillyze@users.noreply.github.com>
2025-03-26 17:06:57 +0900

    이미지 최적화

--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTSavePhotoDelegate.m
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTSavePhotoDelegate.m
@@ -36,46 +36,78 @@
     typeof(self) strongSelf = weakSelf;
     if (!strongSelf) return;
 
+    NSDate *totalStartTime = [NSDate date];
+    
+    // 1. 이미지 데이터 가져오기
+    NSDate *dataStartTime = [NSDate date];
     NSData *photoData = photoDataProvider();
+    NSTimeInterval dataTime = [[NSDate date] timeIntervalSinceDate:dataStartTime];
+    NSLog(@"1. 이미지 데이터 가져오기: %.3f초", dataTime);
+    
+    // 2. UIImage 생성 및 리사이즈를 한번에 처리
+    NSDate *processStartTime = [NSDate date];
     UIImage *image = [UIImage imageWithData:photoData];
     
-    if (image) {
-      CGFloat maxWidth = 1000.0;
-      CGFloat scale = 1.0;
-      
-      if (image.size.width > maxWidth) {
-        scale = maxWidth / image.size.width;
-      }
-      
-      if (scale < 1.0) {
-        CGSize newSize = CGSizeMake(image.size.width * scale, image.size.height * scale);
-        UIGraphicsBeginImageContextWithOptions(newSize, NO, 1.0);
-        [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
-        UIImage *resizedImage = UIGraphicsGetImageFromCurrentImageContext();
+    // 이미지 방향 정규화
+    if (image.imageOrientation != UIImageOrientationUp) {
+        UIGraphicsBeginImageContextWithOptions(image.size, NO, image.scale);
+        [image drawInRect:CGRectMake(0, 0, image.size.width, image.size.height)];
+        image = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
-        
-        // 압축된 이미지를 JPEG로 저장
-        NSData *compressedData = UIImageJPEGRepresentation(resizedImage, 0.8);
-        NSError *ioError;
-        if ([compressedData writeToFile:strongSelf.path options:NSDataWritingAtomic error:&ioError]) {
-          strongSelf.completionHandler(self.path, nil);
-        } else {
-          strongSelf.completionHandler(nil, ioError);
-        }
-      } else {
-        // 원본 크기가 1000px 이하인 경우 그대로 저장
-        NSError *ioError;
-        if ([photoData writeToFile:strongSelf.path options:NSDataWritingAtomic error:&ioError]) {
-          strongSelf.completionHandler(self.path, nil);
-        } else {
-          strongSelf.completionHandler(nil, ioError);
-        }
-      }
+    }
+    
+    // 이미지 크롭 처리
+    CGFloat imageWidth = image.size.width * image.scale;
+    CGFloat imageHeight = image.size.height * image.scale;
+    CGFloat imageRatio = imageWidth / imageHeight;
+    CGFloat previewRatio = 3.0 / 4.0; // 4:3 비율
+    
+    CGRect cropRect;
+    if (imageRatio > previewRatio) {
+        CGFloat newWidth = imageHeight * previewRatio;
+        CGFloat xOffset = (imageWidth - newWidth) / 2;
+        cropRect = CGRectMake(xOffset, 0, newWidth, imageHeight);
     } else {
-      NSError *ioError = [NSError errorWithDomain:NSCocoaErrorDomain
-                                           code:NSURLErrorUnknown
-                                       userInfo:@{NSLocalizedDescriptionKey: @"Failed to create image from photo data"}];
-      strongSelf.completionHandler(nil, ioError);
+        CGFloat newHeight = imageWidth / previewRatio;
+        CGFloat yOffset = (imageHeight - newHeight) / 2;
+        cropRect = CGRectMake(0, yOffset, imageWidth, newHeight);
+    }
+    
+    CGImageRef croppedCGImage = CGImageCreateWithImageInRect(image.CGImage, cropRect);
+    UIImage *croppedImage = [UIImage imageWithCGImage:croppedCGImage scale:image.scale orientation:image.imageOrientation];
+    CGImageRelease(croppedCGImage);
+    
+    // 리사이즈 처리
+    CGFloat maxWidth = 1000.0;
+    CGFloat scale = 1.0;
+    
+    if (croppedImage.size.width > maxWidth) {
+        scale = maxWidth / croppedImage.size.width;
+    }
+    
+    UIImage *finalImage = croppedImage;
+    if (scale < 1.0) {
+        CGSize newSize = CGSizeMake(croppedImage.size.width * scale, croppedImage.size.height * scale);
+        UIGraphicsBeginImageContextWithOptions(newSize, NO, 1.0);
+        [croppedImage drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+        finalImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+    }
+    NSTimeInterval processTime = [[NSDate date] timeIntervalSinceDate:processStartTime];
+    NSLog(@"2. 이미지 처리: %.3f초", processTime);
+    
+    // 3. JPEG 압축 및 저장
+    NSDate *saveStartTime = [NSDate date];
+    NSData *finalData = UIImageJPEGRepresentation(finalImage, 0.8);
+    NSError *ioError;
+    if ([finalData writeToFile:strongSelf.path options:NSDataWritingAtomic error:&ioError]) {
+        NSTimeInterval saveTime = [[NSDate date] timeIntervalSinceDate:saveStartTime];
+        NSTimeInterval totalTime = [[NSDate date] timeIntervalSinceDate:totalStartTime];
+        NSLog(@"3. 파일 저장: %.3f초", saveTime);
+        NSLog(@"총 소요 시간: %.3f초", totalTime);
+        strongSelf.completionHandler(self.path, nil);
+    } else {
+        strongSelf.completionHandler(nil, ioError);
     }
   });
 }
